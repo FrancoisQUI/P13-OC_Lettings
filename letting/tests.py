0333625443e11052429ae11ff5e2b92ac055f2e5
FranQUI <fquivron@gmail.com>
2022-03-22 21:28:38 +0100

    Add tests for detail pages (letting and profile)

--- letting/tests.py
+++ letting/tests.py
@@ -1,11 +1,10 @@
 from django.test import TestCase, Client
 from django.urls import reverse
 
+from .models import Address, Letting
 
-class SiteTestCase(TestCase):
-    def setUp(self):
-        pass
 
+class LettingIndexTestCase(TestCase):
     def test_letting_index_status_code_should_be_200(self):
         client = Client()
         path = reverse("letting:index")
@@ -17,3 +16,29 @@ class SiteTestCase(TestCase):
         path = reverse("letting:index")
         response = client.get(path)
         assert str(response.content).find("<title>Lettings</title>") > 0
+
+
+class LettingDetailTestCase(TestCase):
+    def setUp(self):
+        address = Address.objects.create(number=99,
+                                         street="test_street",
+                                         city='test_city',
+                                         state='XX',
+                                         zip_code=99999,
+                                         country_iso_code='XXX'
+                                         )
+        Letting.objects.create(title="test_letting", address=address)
+
+    def test_letting_detail_status_code_should_be_200(self):
+        letting = Letting.objects.get(title="test_letting")
+        client = Client()
+        path = reverse("letting:letting", kwargs={"letting_id": letting.id})
+        response = client.get(path)
+        assert response.status_code == 200
+
+    def test_letting_detail_page_should_have_title(self):
+        letting = Letting.objects.get(title="test_letting")
+        client = Client()
+        path = reverse("letting:letting", kwargs={"letting_id": letting.id})
+        response = client.get(path)
+        assert str(response.content).find(f"<h1>{letting.title}</h1>") > 0
--- profile/tests.py
+++ profile/tests.py
@@ -1,13 +1,11 @@
 from django.test import TestCase, Client
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 from .models import Profile
 
 
-class SiteTestCase(TestCase):
-    def setUp(self):
-        pass
-
+class ProfileIndexTestCase(TestCase):
     def test_profiles_index_status_code_should_be_200(self):
         client = Client()
         path = reverse("profile:index")
@@ -19,3 +17,22 @@ class SiteTestCase(TestCase):
         path = reverse("profile:index")
         response = client.get(path)
         assert str(response.content).find("<title>Profiles</title>") > 0
+
+class ProfileDetailTestCase(TestCase):
+    def setUp(self):
+        user = User.objects.create(username="test_user")
+        Profile.objects.create(user=user, favorite_city="test_user")
+
+    def test_profile_detail_status_code_should_be_200(self):
+        profile = Profile.objects.get(user__username="test_user")
+        client = Client()
+        path = reverse("profile:profile", kwargs={"username": profile.user.username})
+        response = client.get(path)
+        assert response.status_code == 200
+
+    def test_profiles_detail_page_should_have_title(self):
+        profile = Profile.objects.get(user__username="test_user")
+        client = Client()
+        path = reverse("profile:profile", kwargs={"username": profile.user.username})
+        response = client.get(path)
+        assert str(response.content).find(f"<title>{profile.user.username}</title>") > 0
